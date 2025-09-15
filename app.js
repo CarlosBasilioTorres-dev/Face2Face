6ed98352e74e5ef7cd4a64c164f3075c92434cbe
CarlosBasilioTorres-dev <carlos.basilio.torres@gmail.com>
2025-09-15 02:32:39 -0600

    prueba de formulario

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js")
+        .then(() => alert("SW registrado"))
+        .catch(err => console.error("Error SW:", err));
+}else{
+    const el = document.documentElement
+        el.requestFullscreen();
+}
+
 const sections = {
     home: "home-card",
     sync: "sync-card",
